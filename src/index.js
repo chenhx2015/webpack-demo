719a2225ea8d172ffd3836154ebaba0030018002
tzm <tzm@MacBook-fujitsu.local>
2019-11-16 16:58:41 +0800

    动态导入

--- src/index.js
+++ src/index.js
@@ -1,45 +1,18 @@
-// import _ from 'lodash'; // 先用不到了
+// import _ from 'lodash'; // 换成动态导入的方式
 import { cube } from './math';
 import printMe from './print';
 
 import './style.css';
-// import Icon from './ali.jpg'; // file-loader 会处理
 
-function component() {
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
-    // todo 了解 Lodash 
-    // element.innerHTML = _.join(['Hello', 'Webpack'], ' '); // 先用不到了
-    element.innerHTML = [
-        'Hello webpack!',
-        '5 cubed is equal to ' + cube(5)
-    ].join('\n\n');
-
-    // element.classList.add('hello');
-
-    // 将图片添加到现有的标签里面 
-    // var myIcon = new Image();
-    // myIcon.src = Icon;
-    // element.appendChild(myIcon);
-
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-    element.appendChild(btn);
-
-    return element;
+function getComponent() {
+    // 在注释中使用了 webpackChunkName。这样做会导致我们的 bundle 被命名为 lodash.bundle.js ，而不是 [id].bundle.js
+    return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
+        var element = document.createElement('div');
+        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+        return element;
+    }).catch(error => 'An error occurred while loading the component');
 }
 
-// document.body.appendChild(component());
-
-let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
-document.body.appendChild(element);
-
-if (module.hot) {
-    module.hot.accept('./print.js', function() {
-        console.log('Accepting the updated printMe module');
-        // printMe();
-        document.body.removeChild(element);
-        element = component(); // 重新渲染页面后，component 更新 click 事件处理
-        document.body.appendChild(element);
-    })
-}
+getComponent().then(component => {
+    document.body.appendChild(component);
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 const path = require('path');
 const HtmlWebpacklugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 
 // webpack 最出色的功能之一就是：除了 javascript,还可以通过 loader 引入任何其他类型的文件
 // 在 entry 添加 src/print.js 作为新的入口起点（print），然后修改 output，以便根据入口起点名称动态生成 bundle 名称
 module.exports = {
     entry: {
-        app: './src/index.js',
-        another: './src/another-module.js'
+        app: './src/index.js'
     },
     devtool: 'inline-source-map', // 不要用于生产环境，将编译后的代码映射回原始源代码，为了更容易地追踪错误和警告
     // 告知 webpack-dev-server，在 localhost:8080 下建立服务，将 dist 目录下的文件，作为可访问文件,再添加start脚本直接运行开发服务器
@@ -32,22 +30,9 @@ module.exports = {
             title: 'code splitting'
         })
     ],
-    // 请注意代码分割里面的防止重复，需要这样配置，中文官方文档还是使用老的方法在 plugins里面配置，会报错
-    // @todo 此配置可仔细研究其配置的参数
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                    name: 'commons',
-                    chunks: 'initial',
-                    minChunks: 2
-                }
-            }
-        }
-    },
     output: {
-        // filename: 'bundle.js',
         filename: '[name].bundle.js',
+        chunkFilename: '[name].bundle.js', // 更多相关的可查看 output 相关文档
         path: path.resolve(__dirname, 'dist') // todo path.resolve 方法
     }
 }
